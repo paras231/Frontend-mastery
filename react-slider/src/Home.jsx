c192bfecb11916365e2f94145316a5e0d781e9fd
paras231 <sharmaparas553@gmail.com>
2022-08-23 13:04:00 +0530

    updated

--- react-slider/src/Home.jsx
+++ react-slider/src/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./Home.css";
 const images = [
   "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
   "https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
@@ -9,88 +10,90 @@ const images = [
   "https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
   "https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
   "https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550353175-a3611868086b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550330039-a54e15ed9d33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+ 
+  
 ];
 
+const PrevButton = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" ,marginLeft:"-4vw",marginTop:"-10vh"}}
+      onClick={onClick}
+    />
+  );
+};
 
-
-const prevButton = ()=>{
-
-}
-
-const NextButton = (props)=>{
-    const { className, style, onClick } = props;
-    return (
-        <div
-          className={className}
-          style={{ ...style, display: "block", background: "red" }}
-          onClick={onClick}
-        />
-      );
-}
+const NextButton = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red",marginTop:"-10vh" }}
+      onClick={onClick}
+    />
+  );
+};
 const settings = {
-  className: "slider variable-width",
-  dots: true,
+
+  dots:true,
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 2,
   centerMode: true,
+  prevArrow: <PrevButton />,
   nextArrow: <NextButton />,
   responsive: [
     {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 6,
-          slidesToScroll: 6,
-          infinite: true,
-          dots: true
-        }
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true,
       },
-   
+    },
+
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 4,
+        slidesToShow: 2,
         slidesToScroll: 2,
-        initialSlide: 3
-      }
+        initialSlide: 3,
+      },
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 1
-      }
-    }
-  ]
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 };
 const Home = () => {
   return (
     <>
-    <div style={{width:"100%",overflow:'hidden'}}>
-        <button>Prev</button>
-      <Slider {...settings}>
-        {images.map((i, index) => {
-          return (
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                gap:'20px'
-              }}
-              key={index}
-            >
-              <img style={{ width: "25vw", height: "30vh" }} src={i} />
-            </div>
-          );
-        })}
-      </Slider>
+      <div style={{ width: "80%", margin: "0 auto",marginTop:'10vh' }}>
+        <Slider {...settings}>
+          {images.map((i, index) => {
+            return (
+              <div
+                key={index}
+              >
+                <img style={{ width: "25vw", height: "30vh" }} src={i} />
+              </div>
+            );
+          })}
+        </Slider>
       </div>
     </>
   );
