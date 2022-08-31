8b7da3379ac2bef7c1d1bf3414489421343f4d81
paras231 <sharmaparas553@gmail.com>
2022-08-31 18:11:55 +0530

    adde form

--- page-design1/src/components/Form.jsx
+++ page-design1/src/components/Form.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 import styled from "styled-components";
-
+import "./Form.css";
 const FormWrapper = styled.form`
   display: grid;
   gap: 2vmax;
   justify-content: center;
   align-items: center;
-  margin-top: 25vmax;
+ 
 `;
 
 const Input1 = styled.input`
-  width: 13vmax;
-  height: 3vmax;
-  border-radius:12px;
+  width: 280px;
+  height: 50px;
+  border-radius: 12px;
+  font-size: 20px;
 `;
 const Form = () => {
   return (
     <>
-      <FormWrapper>
+      <FormWrapper className="form-wrapper">
+        <Input1
+          className="myinput"
+          placeholder="Enter Name"
+          type="file"
+          capture="environment"
+          onFocus={false}
+        />
+        <Input1 className="myinput" placeholder="Enter Name" type="text" />
+        <Input1 className="myinput" placeholder="Enter Name" type="text" />
         <Input1 className="myinput" placeholder="Enter Name" type="text" />
         <Input1 className="myinput" placeholder="Enter Name" type="text" />
         <Input1 className="myinput" placeholder="Enter Name" type="text" />
