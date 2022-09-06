fda99174765dd160ffeb2095482d6624536aa3ef
paras231 <sharmaparas553@gmail.com>
2022-09-06 22:24:12 +0530

    updated cabs page

--- page-design1/src/Travel/Cabs.jsx
+++ page-design1/src/Travel/Cabs.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import bg from "../assets/travelheader.png";
 import styled from "styled-components";
-import darkline from "../assets/darkline.png";
+import { cabsData } from "./data";
+import tick from "../assets/tick.png";
+import Line from "../assets/line.png";
+import { HiArrowRight } from "react-icons/hi";
+import { HiArrowLeft } from "react-icons/hi";
 const StyledHeader = styled.div``;
 const HeaderImage = styled.img`
   width: 100%;
@@ -56,7 +60,9 @@ const CheckBoxWrapperMain = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
+  gap: 1vmax;
   margin-left: 3vmax;
+  margin-top: 6vmax;
 `;
 
 const CarModalCheckBox = styled.div`
@@ -96,11 +102,167 @@ const CabsMainContainer = styled.div`
   display: flex;
   flex-direction: column;
   flex: 3;
-  background-color: yellow;
-  height: 40vmax;
+  gap: 3vmax;
+`;
+// cabs card container->
+
+const CardMainContainer = styled.div`
+  display: flex;
+  gap: 4vmax;
+  width: 65vmax;
+  height: 23vmax;
+  box-shadow: 10px 11px 25px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  margin-left: 5vmax;
+  @media (min-width: 992px) and (max-width: 1024px) {
+    height: 30vmax;
+    width: 70vmax;
+  }
+`;
+const CardImageWrapper = styled.div`
+  margin-top: 2vmax;
+  margin-left: 2vmax;
+`;
+const CardImage = styled.img`
+  height: 14vmax;
+  width: 14vmax;
+  border-radius: 5px;
 `;
 
+const CardDetailsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const CarName = styled.p`
+  color: #006ce0;
+  font-weight: 500;
+  font-size: 1.6rem;
+  margin-top: 2vmax;
+`;
+
+const CarTitle = styled.p`
+  color: #000000;
+  font-weight: 400;
+  font-size: 1rem;
+  margin-top: 1vmax;
+`;
+const CarSize = styled.p`
+  color: #000000;
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin-top: 4vmax;
+`;
+const CarDesc = styled.div`
+  width: 16vmax;
+  height: 10vmax;
+  font-size: 1rem;
+  margin-top: 1vmax;
+`;
+const Sanitized = styled.div`
+  display: flex;
+  gap: 1vmax;
+  margin-top: -4vmax;
+  @media (min-width: 992px) and (max-width: 1024px) {
+    margin-top: -2vmax;
+  }
+`;
+const CardBookinWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2.5vmax;
+  margin-left: -3vmax;
+`;
+const Cancellation = styled.div`
+  width: 22vmax;
+  height: 1.4vmax;
+  font-size: 1rem;
+  text-align: center;
+  border-radius: 30px;
+  border: 1px solid #0a8607;
+  background: #bdffbc;
+  color: #0a8607;
+  @media (min-width: 992px) and (max-width: 1024px) {
+    width: 28vmax;
+    height: 1.6vmax;
+  }
+`;
+const CancelText = styled.p``;
+
+const Discount = styled.div`
+  background: #ffecec;
+  border: 1px solid #850000;
+  border-radius: 30px;
+  margin-top: 6vmax;
+  text-align: center;
+  width: 13vmax;
+  height: 1.4vmax;
+  margin-left: 7vmax;
+`;
+
+const PriceWrapper = styled.div`
+  margin-top: 4vmax;
+  margin-left: 15vmax;
+  display: flex;
+  gap:1.2vmax;
+`;
+const OldPrice = styled.p`
+  color: #8d8d8d;
+  font-size: 0.8rem;
+`;
+
+const NewPrice = styled.p`
+  color: #000000;
+  font-size: 1.7rem;
+  font-weight: bold;
+  margin-top:-1vmax;
+`;
+
+const Taxes = styled.p`
+  margin-top:2vmax;
+  font-size:1rem;
+  margin-left: 15vmax;
+`
+//  next previous button wrapper->
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 1vmax;
+  margin-top: 4vmax;
+  margin-left: 80%;
+`;
+const PreviousButton = styled.button`
+  height: 2vmax;
+  width: 2.5vmax;
+  background:transparent;
+  border:1px solid black;
+  cursor: pointer;
+`;
+const NexButton = styled.button`
+  height: 2vmax;
+  width: 2.5vmax;
+  background:transparent;
+  border:1px solid black;
+  cursor: pointer;
+`;
 const Cabs = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const [cabsPerPage] = useState(4);
+  const indexOfLastCab = currentPage + cabsPerPage;
+  const indexOfFirstCab = indexOfLastCab - cabsPerPage;
+  const currentCabs = cabsData.slice(indexOfFirstCab, indexOfLastCab);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const next = () => {
+    if (currentPage < Math.ceil(cabsData.length / cabsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  const prev = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  console.log(currentCabs);
   return (
     <>
       <StyledHeader>
@@ -157,9 +319,59 @@ const Cabs = () => {
             </CheckBoxWrapper>
           </CarModalCheckBox>
         </CheckBoxWrapperMain>
-
+        <img src={Line} />
         <CabsMainContainer>
-          <center style={{ fontWeight: "bold" }}>Cabs</center>
+          <center
+            style={{ fontWeight: "bold", color: "#145843", fontSize: "1.5rem" }}
+          >
+            Cabs
+          </center>
+          {currentCabs.map((value) => {
+            return (
+              <>
+                <CardMainContainer key={value.id}>
+                  <CardImageWrapper>
+                    <CardImage src={value.image} />
+                  </CardImageWrapper>
+                  <CardDetailsWrapper>
+                    <CarName>{value.name}</CarName>
+                    <CarTitle>{value.title}</CarTitle>
+                    <CarSize>{value.size}</CarSize>
+                    <CarDesc>{value.desc}</CarDesc>
+                    <Sanitized>
+                      <img style={{ height: "1.4vmax" }} src={tick} />
+                      <span style={{ fontSize: "1.2rem" }}>
+                        Sanitizers Installed
+                      </span>
+                    </Sanitized>
+                  </CardDetailsWrapper>
+                  <img src={Line} />
+                  <CardBookinWrapper>
+                    <Cancellation>
+                      Free cancelltion till1 hour of departure
+                    </Cancellation>
+                    <Discount>41% cheaper than usual</Discount>
+                    <PriceWrapper>
+                      <OldPrice>$726</OldPrice>
+                      <NewPrice>
+                      $
+                     {value.price}
+                      </NewPrice>
+                    </PriceWrapper>
+                    <Taxes>$ {value.taxes}Taxes & Fees</Taxes>
+                  </CardBookinWrapper>
+                </CardMainContainer>
+              </>
+            );
+          })}
+          <ButtonWrapper>
+            <PreviousButton onClick={prev}>
+              <HiArrowLeft />
+            </PreviousButton>
+            <NexButton onClick={next}>
+              <HiArrowRight />
+            </NexButton>
+          </ButtonWrapper>
         </CabsMainContainer>
       </MainContainer>
     </>
