d295bdce7cd9384f7b3987ad74a5a192977ea242
paras231 <sharmaparas553@gmail.com>
2022-10-17 21:23:38 +0530

    updated

--- react-firebase-chat/src/Components/Navbar.jsx
+++ react-firebase-chat/src/Components/Navbar.jsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "../styles/navbar.module.scss";
 import { signOut } from "firebase/auth";
-import {auth} from "../Firebase";
+import { auth } from "../Firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
-  const url =
-    "https://images.unsplash.com/photo-1664374170305-31a1630b2038?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60";
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <>
       <div className={styles.navbar}>
         <span className={styles.logo}>React Chat</span>
         <div className={styles.user}>
-          <img src={url} alt="" />
-          <span>Jhon</span>
+          <img src={currentUser.photoURL} alt="url" />
+          <span>{currentUser.displayName}</span>
           <button onClick={() => signOut(auth)}>Logout</button>
         </div>
       </div>
--- react-firebase-chat/src/Components/Search.jsx
+++ react-firebase-chat/src/Components/Search.jsx
@@ -1,21 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/home.module.scss";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../Firebase";
 
 const Search = () => {
+  const [userName, setUsrename] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", userName)
+    );
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+        console.log(doc.id, " => ", doc.data());
+      });
+    } catch (error) {
+      setErr(true);
+      console.log(error);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
   const url =
     "https://images.unsplash.com/photo-1664374170305-31a1630b2038?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60";
   return (
     <>
       <div className={styles.search}>
         <div className={styles.searchForm}>
-          <input type="text" placeholder="Find a user" />
+          <input
+            type="text"
+            placeholder="Find a user"
+            onKeyDown={handleKey}
+            onChange={(e) => setUsrename(e.target.value)}
+            value={userName}
+          />
         </div>
-        <div className={styles.userChat}>
-          <img src={url} alt="" />
-          <div className={styles.userchatInfo}>
-            <span>Jane</span>
+        {err && <span>User not found</span>}
+        {user && (
+          <div className={styles.userChat}>
+            <img src={user.photoURL} alt="" />
+            <div className={styles.userchatInfo}>
+              <span>{user.displayName}</span>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </>
   );
