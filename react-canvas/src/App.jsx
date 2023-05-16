1c23c7490b9b899ad28115e17389df9342ffcef9
paras231 <sharmaparas553@gmail.com>
2023-05-16 22:51:21 +0530

    added reactcanvas drwaing

--- react-canvas/src/App.jsx
+++ react-canvas/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import MainComponent from "./components/MainComponent";
-
+import CanvasPaint from "./CanvasPaint";
 const App = () => {
   return (
     <>
-     <MainComponent/>
+     {/* <MainComponent/> */}
+     <CanvasPaint/>
     </>
   );
 };
---
+++ react-canvas/src/CanvasPaint.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactCanvasPaint from "react-canvas-paint";
+import "react-canvas-paint/dist/index.css";
+
+const CanvasPaint = () => {
+  return (
+    <>
+      <h1>ReactJs Drawing Canvas</h1>
+      <ReactCanvasPaint />
+    </>
+  );
+};
+
+export default CanvasPaint;
